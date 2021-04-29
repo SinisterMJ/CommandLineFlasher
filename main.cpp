447422c7653f97cb9e8b63c498cbabff36e25431
Anton Roth <Anton_Roth@gmx.de>
2021-04-29 12:35:08 +0200

    Updated code to properly flash the drives now, both mounted and unmounted. Also included error bitflag if writing to output file (SD Card) failed.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,55 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <codecvt>
+
+#include <Windows.h>
+
+bool unmountDrive(std::string path)
+{
+    // First get a lock on the device.
+    // This needs to be done as low-level C, and does not come in C++
+    
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    std::wstring wide = converter.from_bytes(path);
+
+    HANDLE volume = CreateFile(wide.c_str(), GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (volume == INVALID_HANDLE_VALUE)
+        return false;
+
+    // Get a lock on the volume
+    DWORD bytesreturned;
+    auto result = DeviceIoControl(volume, FSCTL_LOCK_VOLUME, NULL, 0, NULL, 0, &bytesreturned, NULL);
+
+    if (!result)
+        return false;
+
+    // Unmount volume
+    DWORD junk;
+    result = DeviceIoControl(volume, FSCTL_DISMOUNT_VOLUME, NULL, 0, NULL, 0, &junk, NULL);
+    if (!result)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 
 int main(int argc, char *argv[])
 {
-    if ( argc != 3 )
+    if ( argc < 3 )
     {
         return 1; //not enough arguments
+    }
+
+    if (argc == 4)
+    {
+        std::string path(argv[3]);
+        path = "\\\\.\\" + path + ":";
+        if (!unmountDrive(path))
+            return 4; // could not unmount
     }
 
     std::string drive = std::string(argv[2]);
@@ -40,6 +83,10 @@ int main(int argc, char *argv[])
         accumulated += read;
 
         out.write(buffer.data(), read);
+
+        int error = out.rdstate();
+        if (error != std::ios::goodbit)
+            return 3;
     }
 
     out.flush();
